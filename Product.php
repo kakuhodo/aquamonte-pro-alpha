20a04d40f8a586fa23d8bbfc0708b865f4f07e26
princepink <git@pearlpuppy.com>
2024-03-05 04:42:37 +0900

    refined hooks

--- Product.php
+++ Product.php
@@ -42,26 +42,6 @@ class Product extends Abs_Theme
     /**
      *
      */
-    public function hookActionAdminHead()
-    {
-        # !![ISSUE] use fontawesome in admin -->>> should be config
-        echo '<script src="https://kit.fontawesome.com/158897db51.js" crossorigin="anonymous"></script>' . PHP_EOL;
-    }
-
-    /**
-     *
-     */
-    public function hookActionAfterSetupTheme()
-    {
-        if (is_child_theme()) {
-            add_editor_style(get_template_directory_uri() . '/editor-style.css');
-        }
-        add_editor_style('editor-style.css');
-    }
-
-    /**
-     *
-     */
 
     /**
      *
