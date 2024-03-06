86096e255f2f551322d1849c19d2d6ac4f74abee
princepink <git@pearlpuppy.com>
2024-03-06 20:10:00 +0900

    completed assets autoload

--- page-sandy.php
+++ page-sandy.php
@@ -18,15 +18,41 @@ get_header();
 
 /**
  *
- *
+ *  hookActionAdminEnqueueScripts
 $hoge = parse_ini_file('support.ini', true, INI_SCANNER_TYPED);
 
 /**
  *
  */
+$str = 'hookActionAdminEnqueueScripts';
+
+preg_match('/Action(\w+)Enqueue/', $str, $matches);
+
+/**
+ *
+ */
+
+/**
+ *
+ */
+
+/**
+ *
+ */
+
+$am->slap($matches, '_is');
+$am->slap(wp_get_theme()->get('stylesheet'));
+$am->slap(wp_style_is('cocore'), '_EQ');
+$am->slap($am, 'AM Product');
+$am->consExpose();
+
+/**
+ *
+ *
 ?>
 <pre><code><?php print_r($am); ?></code></pre>
-
+*/
+?>
 </main><!--//#container-->
 
 <?php get_footer(); ?>
